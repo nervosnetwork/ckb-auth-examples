ccc7d74a33fedebde71e5f79c3d3cb5f84aad9a6
joii2020 <joii@cryptape.com>
2023-12-19 14:29:27 +0800

    Support a transaction multiple signatures

--- C/auth_c_example.c
+++ C/auth_c_example.c
@@ -1,8 +1,13 @@
-// a demo to use "auth" lib
-// script args: <21 bytes auth> <32 bytes code hash> <1 byte hash type> <1 byte
-// entry category> see `CkbEntryType`
+// This on-chain script is for testing purposes:
+// Using multiple different versions of ckb-auth within a single transaction.
 //
-// witness lock: signature
+// Script Args:
+// In Args, there will be multiple sets of args for auth_c_lock.c.
+//  The first element in Args indicates the number of sets,
+//    so the length of Args is 1 + n * 54.
+//
+// Witness:
+// Corresponding ckb_auth for each set sequentially.
 
 #include "blake2b.h"
 #include "blockchain.h"
@@ -17,210 +22,235 @@
 
 #define ONE_BATCH_SIZE 32768
 
+#define CKB_AUTH_ARGS_SIZE (CKB_AUTH_LEN + BLAKE2B_BLOCK_SIZE + 1 + 1)
+
 static int extract_witness_lock(uint8_t *witness, uint64_t len,
                                 mol_seg_t *lock_bytes_seg) {
-  if (len < 20) {
-    return CKB_INVALID_DATA;
-  }
-  uint32_t lock_length = *((uint32_t *)(&witness[16]));
-  if (len < 20 + lock_length) {
-    return CKB_INVALID_DATA;
-  } else {
-    lock_bytes_seg->ptr = &witness[20];
-    lock_bytes_seg->size = lock_length;
-  }
-  return CKB_SUCCESS;
+    if (len < 20) {
+        return CKB_INVALID_DATA;
+    }
+    uint32_t lock_length = *((uint32_t *)(&witness[16]));
+    if (len < 20 + lock_length) {
+        return CKB_INVALID_DATA;
+    } else {
+        lock_bytes_seg->ptr = &witness[20];
+        lock_bytes_seg->size = lock_length;
+    }
+    return CKB_SUCCESS;
 }
 
 int load_and_hash_witness(blake2b_state *ctx, size_t start, size_t index,
                           size_t source, bool hash_length) {
-  uint8_t temp[ONE_BATCH_SIZE];
-  uint64_t len = ONE_BATCH_SIZE;
-  int ret = ckb_load_witness(temp, &len, start, index, source);
-  if (ret != CKB_SUCCESS) {
-    return ret;
-  }
-  if (hash_length) {
-    blake2b_update(ctx, (char *)&len, sizeof(uint64_t));
-  }
-  uint64_t offset = (len > ONE_BATCH_SIZE) ? ONE_BATCH_SIZE : len;
-  blake2b_update(ctx, temp, offset);
-  while (offset < len) {
-    uint64_t current_len = ONE_BATCH_SIZE;
-    ret = ckb_load_witness(temp, &current_len, start + offset, index, source);
+    uint8_t temp[ONE_BATCH_SIZE];
+    uint64_t len = ONE_BATCH_SIZE;
+    int ret = ckb_load_witness(temp, &len, start, index, source);
     if (ret != CKB_SUCCESS) {
-      return ret;
+        return ret;
     }
-    uint64_t current_read =
-        (current_len > ONE_BATCH_SIZE) ? ONE_BATCH_SIZE : current_len;
-    blake2b_update(ctx, temp, current_read);
-    offset += current_read;
-  }
-  return CKB_SUCCESS;
+    if (hash_length) {
+        blake2b_update(ctx, (char *)&len, sizeof(uint64_t));
+    }
+    uint64_t offset = (len > ONE_BATCH_SIZE) ? ONE_BATCH_SIZE : len;
+    blake2b_update(ctx, temp, offset);
+    while (offset < len) {
+        uint64_t current_len = ONE_BATCH_SIZE;
+        ret =
+            ckb_load_witness(temp, &current_len, start + offset, index, source);
+        if (ret != CKB_SUCCESS) {
+            return ret;
+        }
+        uint64_t current_read =
+            (current_len > ONE_BATCH_SIZE) ? ONE_BATCH_SIZE : current_len;
+        blake2b_update(ctx, temp, current_read);
+        offset += current_read;
+    }
+    return CKB_SUCCESS;
 }
 
 int generate_sighash_all(uint8_t *msg, size_t msg_len) {
-  int ret;
-  uint64_t len = 0;
-  unsigned char temp[MAX_WITNESS_SIZE];
-  uint64_t read_len = MAX_WITNESS_SIZE;
-  uint64_t witness_len = MAX_WITNESS_SIZE;
+    int ret;
+    uint64_t len = 0;
+    unsigned char temp[MAX_WITNESS_SIZE];
+    uint64_t read_len = MAX_WITNESS_SIZE;
+    uint64_t witness_len = MAX_WITNESS_SIZE;
 
-  if (msg_len < BLAKE2B_BLOCK_SIZE) {
-    return CKB_INVALID_DATA;
-  }
+    if (msg_len < BLAKE2B_BLOCK_SIZE) {
+        return CKB_INVALID_DATA;
+    }
 
-  /* Load witness of first input */
-  ret = ckb_load_witness(temp, &read_len, 0, 0, CKB_SOURCE_GROUP_INPUT);
-  if (ret != CKB_SUCCESS) {
-    return CKB_INVALID_DATA;
-  }
-  witness_len = read_len;
-  if (read_len > MAX_WITNESS_SIZE) {
-    read_len = MAX_WITNESS_SIZE;
-  }
-
-  /* load signature */
-  mol_seg_t lock_bytes_seg;
-  ret = extract_witness_lock(temp, read_len, &lock_bytes_seg);
-  if (ret != 0) {
-    return CKB_INVALID_DATA;
-  }
-
-  /* Load tx hash */
-  unsigned char tx_hash[BLAKE2B_BLOCK_SIZE];
-  len = BLAKE2B_BLOCK_SIZE;
-  ret = ckb_load_tx_hash(tx_hash, &len, 0);
-  if (ret != CKB_SUCCESS) {
-    return ret;
-  }
-  if (len != BLAKE2B_BLOCK_SIZE) {
-    return CKB_INVALID_DATA;
-  }
-
-  /* Prepare sign message */
-  blake2b_state blake2b_ctx;
-  blake2b_init(&blake2b_ctx, BLAKE2B_BLOCK_SIZE);
-  blake2b_update(&blake2b_ctx, tx_hash, BLAKE2B_BLOCK_SIZE);
-
-  /* Clear lock field to zero, then digest the first witness
-   * lock_bytes_seg.ptr actually points to the memory in temp buffer
-   * */
-  memset((void *)lock_bytes_seg.ptr, 0, lock_bytes_seg.size);
-  blake2b_update(&blake2b_ctx, (char *)&witness_len, sizeof(uint64_t));
-  blake2b_update(&blake2b_ctx, temp, read_len);
-
-  // remaining of first witness
-  if (read_len < witness_len) {
-    ret = load_and_hash_witness(&blake2b_ctx, read_len, 0,
-                                CKB_SOURCE_GROUP_INPUT, false);
+    /* Load witness of first input */
+    ret = ckb_load_witness(temp, &read_len, 0, 0, CKB_SOURCE_GROUP_INPUT);
     if (ret != CKB_SUCCESS) {
-      return CKB_INVALID_DATA;
+        return CKB_INVALID_DATA;
     }
-  }
+    witness_len = read_len;
+    if (read_len > MAX_WITNESS_SIZE) {
+        read_len = MAX_WITNESS_SIZE;
+    }
 
-  // Digest same group witnesses
-  size_t i = 1;
-  while (1) {
-    ret =
-        load_and_hash_witness(&blake2b_ctx, 0, i, CKB_SOURCE_GROUP_INPUT, true);
-    if (ret == CKB_INDEX_OUT_OF_BOUND) {
-      break;
+    /* load signature */
+    mol_seg_t lock_bytes_seg;
+    ret = extract_witness_lock(temp, read_len, &lock_bytes_seg);
+    if (ret != 0) {
+        return CKB_INVALID_DATA;
     }
+
+    /* Load tx hash */
+    unsigned char tx_hash[BLAKE2B_BLOCK_SIZE];
+    len = BLAKE2B_BLOCK_SIZE;
+    ret = ckb_load_tx_hash(tx_hash, &len, 0);
     if (ret != CKB_SUCCESS) {
-      return CKB_INVALID_DATA;
+        return ret;
     }
-    i += 1;
-  }
-
-  // Digest witnesses that not covered by inputs
-  i = (size_t)ckb_calculate_inputs_len();
-  while (1) {
-    ret = load_and_hash_witness(&blake2b_ctx, 0, i, CKB_SOURCE_INPUT, true);
-    if (ret == CKB_INDEX_OUT_OF_BOUND) {
-      break;
+    if (len != BLAKE2B_BLOCK_SIZE) {
+        return CKB_INVALID_DATA;
     }
-    if (ret != CKB_SUCCESS) {
-      return CKB_INVALID_DATA;
+
+    /* Prepare sign message */
+    blake2b_state blake2b_ctx;
+    blake2b_init(&blake2b_ctx, BLAKE2B_BLOCK_SIZE);
+    blake2b_update(&blake2b_ctx, tx_hash, BLAKE2B_BLOCK_SIZE);
+
+    /* Clear lock field to zero, then digest the first witness
+     * lock_bytes_seg.ptr actually points to the memory in temp buffer
+     * */
+    memset((void *)lock_bytes_seg.ptr, 0, lock_bytes_seg.size);
+    blake2b_update(&blake2b_ctx, (char *)&witness_len, sizeof(uint64_t));
+    blake2b_update(&blake2b_ctx, temp, read_len);
+
+    // remaining of first witness
+    if (read_len < witness_len) {
+        ret = load_and_hash_witness(&blake2b_ctx, read_len, 0,
+                                    CKB_SOURCE_GROUP_INPUT, false);
+        if (ret != CKB_SUCCESS) {
+            return CKB_INVALID_DATA;
+        }
     }
-    i += 1;
-  }
 
-  blake2b_final(&blake2b_ctx, msg, BLAKE2B_BLOCK_SIZE);
+    // Digest same group witnesses
+    size_t i = 1;
+    while (1) {
+        ret = load_and_hash_witness(&blake2b_ctx, 0, i, CKB_SOURCE_GROUP_INPUT,
+                                    true);
+        if (ret == CKB_INDEX_OUT_OF_BOUND) {
+            break;
+        }
+        if (ret != CKB_SUCCESS) {
+            return CKB_INVALID_DATA;
+        }
+        i += 1;
+    }
 
-  return 0;
+    // Digest witnesses that not covered by inputs
+    i = (size_t)ckb_calculate_inputs_len();
+    while (1) {
+        ret = load_and_hash_witness(&blake2b_ctx, 0, i, CKB_SOURCE_INPUT, true);
+        if (ret == CKB_INDEX_OUT_OF_BOUND) {
+            break;
+        }
+        if (ret != CKB_SUCCESS) {
+            return CKB_INVALID_DATA;
+        }
+        i += 1;
+    }
+
+    blake2b_final(&blake2b_ctx, msg, BLAKE2B_BLOCK_SIZE);
+
+    return 0;
 }
 
 int main() {
-  int ret;
-  uint64_t len = 0;
-  unsigned char temp[TEMP_SIZE];
+    int ret;
+    uint64_t len = 0;
+    unsigned char temp[TEMP_SIZE];
 
-  unsigned char script[SCRIPT_SIZE];
-  len = SCRIPT_SIZE;
-  ret = ckb_load_script(script, &len, 0);
-  if (ret != CKB_SUCCESS) {
-    return CKB_INVALID_DATA;
-  }
-  if (len > SCRIPT_SIZE) {
-    return CKB_INVALID_DATA;
-  }
-  mol_seg_t script_seg;
-  script_seg.ptr = (uint8_t *)script;
-  script_seg.size = len;
+    unsigned char script[SCRIPT_SIZE];
+    len = SCRIPT_SIZE;
+    ret = ckb_load_script(script, &len, 0);
+    if (ret != CKB_SUCCESS) {
+        return CKB_INVALID_DATA;
+    }
+    if (len > SCRIPT_SIZE) {
+        return CKB_INVALID_DATA;
+    }
+        mol_seg_t script_seg;
+    script_seg.ptr = (uint8_t *)script;
+    script_seg.size = len;
 
-  if (MolReader_Script_verify(&script_seg, false) != MOL_OK) {
-    return CKB_INVALID_DATA;
-  }
+    if (MolReader_Script_verify(&script_seg, false) != MOL_OK) {
+        return CKB_INVALID_DATA;
+    }
 
-  mol_seg_t args_seg = MolReader_Script_get_args(&script_seg);
-  mol_seg_t args_bytes_seg = MolReader_Bytes_raw_bytes(&args_seg);
-  if (args_bytes_seg.size != (21 + 32 + 1 + 1)) {
-    return CKB_INVALID_DATA;
-  }
+        mol_seg_t args_seg = MolReader_Script_get_args(&script_seg);
+    mol_seg_t args_bytes_seg = MolReader_Bytes_raw_bytes(&args_seg);
 
-  // Load the first witness, or the witness of the same index as the first input
-  // using current script.
-  uint64_t witness_len = MAX_WITNESS_SIZE;
-  ret = ckb_load_witness(temp, &witness_len, 0, 0, CKB_SOURCE_GROUP_INPUT);
-  if (ret != CKB_SUCCESS) {
-    return CKB_INVALID_DATA;
-  }
+    if (args_bytes_seg.size < 1) {
+        return CKB_INVALID_DATA;
+    }
+        size_t auth_num = args_bytes_seg.ptr[0];
+    // auth 21bytes + code_hash 32bytes + hash_type 1byte + entry_category 1byte
+    if (auth_num == 0 ||
+        args_bytes_seg.size != auth_num * CKB_AUTH_ARGS_SIZE + 1) {
+        return CKB_INVALID_DATA;
+    }
+    
+    // Load the first witness, or the witness of the same index as the first
+    // input using current script.
+    uint64_t witness_len = MAX_WITNESS_SIZE;
+    ret = ckb_load_witness(temp, &witness_len, 0, 0, CKB_SOURCE_GROUP_INPUT);
+    if (ret != CKB_SUCCESS) {
+        return CKB_INVALID_DATA;
+    }
+    
+    mol_seg_t lock_bytes_seg;
+    ret = extract_witness_lock(temp, witness_len, &lock_bytes_seg);
+    if (ret != 0) {
+        return CKB_INVALID_DATA;
+    }
+        uint8_t msg32[32];
+    ret = generate_sighash_all(msg32, 32);
+    if (ret != 0) return CKB_INVALID_DATA;
 
-  mol_seg_t lock_bytes_seg;
-  ret = extract_witness_lock(temp, witness_len, &lock_bytes_seg);
-  if (ret != 0) {
-    return CKB_INVALID_DATA;
-  }
-  uint8_t msg32[32];
-  ret = generate_sighash_all(msg32, 32);
-  if (ret != 0)
-    return CKB_INVALID_DATA;
+        size_t witness_offset = 0;
+    for (size_t i = 0; i < auth_num; i++) {
+        size_t args_offset = 1 + CKB_AUTH_ARGS_SIZE * i;
+        uint8_t *args = args_bytes_seg.ptr + args_offset;
 
-  CkbEntryType entry;
-  memcpy(entry.code_hash, args_bytes_seg.ptr + 21, 32);
-  entry.hash_type = *(args_bytes_seg.ptr + 21 + 32);
-  entry.entry_category = *(args_bytes_seg.ptr + 21 + 32 + 1);
+        CkbAuthType auth;
+        auth.algorithm_id = args[0];
+        memcpy(&auth.content, args + 1, AUTH160_SIZE);
 
-  CkbAuthType auth;
-  auth.algorithm_id = *args_bytes_seg.ptr;
-  memcpy(auth.content, args_bytes_seg.ptr + 1, 20);
+        CkbEntryType entry;
+        memcpy(entry.code_hash, args + CKB_AUTH_LEN, BLAKE2B_BLOCK_SIZE);
+        entry.hash_type = *(args + CKB_AUTH_LEN + BLAKE2B_BLOCK_SIZE);
+        entry.entry_category = *(args + CKB_AUTH_LEN + BLAKE2B_BLOCK_SIZE + 1);
 
-  // Due to the variable length of certain Lock signs, 
-  //  it becomes difficult to determine the CKB sign message. 
-  //  Therefore, additional data needs to be appended later.
-  //  It is necessary to remove this data at this point.
-  switch (auth.algorithm_id) {
-    case AuthAlgorithmIdRipple:
-      if (lock_bytes_seg.ptr[lock_bytes_seg.size - 1] >= lock_bytes_seg.size) {
-          return 102;  // ERROR_INVALID_ARG
-      }
-      lock_bytes_seg.size -= lock_bytes_seg.ptr[lock_bytes_seg.size - 1];
-      break;
-    default:
-      break;
-  }
+        if (lock_bytes_seg.size < witness_offset + 2) {
+            return CKB_INVALID_DATA;
+        }
+        uint32_t sign_size = *(uint16_t *)(lock_bytes_seg.ptr + witness_offset);
+        if (lock_bytes_seg.size < witness_offset + 2 + sign_size) {
+            return CKB_INVALID_DATA;
+        }
+        uint8_t *signature = lock_bytes_seg.ptr + witness_offset + 2;
 
-  return ckb_auth(&entry, &auth, lock_bytes_seg.ptr, lock_bytes_seg.size,
-                  msg32);
+        switch (auth.algorithm_id) {
+            case AuthAlgorithmIdRipple:
+                if (lock_bytes_seg.ptr[lock_bytes_seg.size - 1] >=
+                    lock_bytes_seg.size) {
+                    return 102;  // ERROR_INVALID_ARG
+                }
+                lock_bytes_seg.size -=
+                    lock_bytes_seg.ptr[lock_bytes_seg.size - 1];
+                break;
+            default:
+                break;
+        }
+        witness_offset += sign_size + 2;
+
+        ret = ckb_auth(&entry, &auth, signature, sign_size, msg32);
+        if (ret) {
+            return ret;
+        }
+    }
 }
--- Rust/contracts/auth-rust-example/src/entry.rs
+++ Rust/contracts/auth-rust-example/src/entry.rs
@@ -10,6 +10,7 @@ use core::result::Result;
 
 use crate::error::Error;
 
+use alloc::vec::Vec;
 use ckb_auth_rs::{
     ckb_auth, generate_sighash_all, AuthAlgorithmIdType, CkbAuthError, CkbAuthType, CkbEntryType,
     EntryCategoryType,
@@ -19,28 +20,57 @@ use ckb_std::{
     ckb_types::{bytes::Bytes, core::ScriptHashType, prelude::*},
     high_level::{load_script, load_witness_args},
 };
+use core::mem::size_of;
+
+const AUTH160_SIZE: usize = 20;
+const CKB_AUTH_SIZE: usize = 1 + AUTH160_SIZE; // 21
+const BLAKE2B_BLOCK_SIZE: usize = 32;
+
+// ckb_auth + code_hash + hash_type + entry_type
+const CKB_AUTH_ARGS_SIZE: usize = CKB_AUTH_SIZE + BLAKE2B_BLOCK_SIZE + 1 + 1;
 
 // use ckb_std::debug;
 
-pub fn main() -> Result<(), Error> {
-    let mut pubkey_hash = [0u8; 20];
-    let auth_id: u8;
-    let entry_type: u8;
-    let hash_type: ScriptHashType;
-    let mut code_hash = [0u8; 32];
+fn get_transaction_info() -> Result<(Vec<u8>, Vec<u8>, [u8; 32]), Error> {
+    let script = load_script()?;
+    let args: Bytes = script.args().unpack();
 
-    // get message
+    let witness_args = load_witness_args(0, Source::GroupInput).map_err(|_| Error::WitnessError)?;
+    let witness = witness_args
+        .lock()
+        .to_opt()
+        .ok_or(CkbAuthError::SignatureMissing)?
+        .raw_data();
+
     let message = generate_sighash_all().map_err(|_| Error::GeneratedMsgError)?;
-    let signature = {
-        let script = load_script()?;
-        let args: Bytes = script.args().unpack();
-        if args.len() != 55 {
-            return Err(Error::ArgsError);
-        }
-        auth_id = args[0] as u8;
-        pubkey_hash.copy_from_slice(&args[1..21]);
-        code_hash.copy_from_slice(&args[21..53]);
-        hash_type = match args[53] as u8 {
+
+    Ok((args.to_vec(), witness.to_vec(), message))
+}
+
+pub fn main() -> Result<(), Error> {
+    let (args, witness, message) = get_transaction_info()?;
+
+    if args.len() < 1 {
+        return Err(Error::ArgsError);
+    }
+    let auth_count = args[0] as usize;
+    if args.len() != 1 + CKB_AUTH_ARGS_SIZE * auth_count {
+        return Err(Error::ArgsError);
+    }
+
+    let mut witness_offset = 0usize;
+    for index in 0..auth_count {
+        let args_offset = 1 + index * CKB_AUTH_ARGS_SIZE;
+        let auth_id = args[args_offset];
+        let pubkey_hash: [u8; AUTH160_SIZE] = args[args_offset + 1..args_offset + 1 + AUTH160_SIZE]
+            .try_into()
+            .unwrap();
+
+        let code_hash: [u8; BLAKE2B_BLOCK_SIZE] = args
+            [args_offset + 1 + AUTH160_SIZE..args_offset + 1 + AUTH160_SIZE + BLAKE2B_BLOCK_SIZE]
+            .try_into()
+            .unwrap();
+        let hash_type = match args[args_offset + 1 + AUTH160_SIZE + BLAKE2B_BLOCK_SIZE] {
             0 => ScriptHashType::Data,
             1 => ScriptHashType::Type,
             2 => ScriptHashType::Data1,
@@ -48,31 +78,42 @@ pub fn main() -> Result<(), Error> {
                 return Err(Error::ArgsError);
             }
         };
-        entry_type = args[54] as u8;
+        let entry_type = args[args_offset + 1 + AUTH160_SIZE + BLAKE2B_BLOCK_SIZE + 1] as u8;
 
-        let witness_args =
-            load_witness_args(0, Source::GroupInput).map_err(|_| Error::WitnessError)?;
-        witness_args
-            .lock()
-            .to_opt()
-            .ok_or(CkbAuthError::SignatureMissing)?
-            .raw_data()
-    };
+        let signature = {
+            if witness.len() < witness_offset + size_of::<u16>() {
+                return Err(Error::ArgsError);
+            }
+            let sign_size = u16::from_le_bytes(
+                witness[witness_offset..witness_offset + size_of::<u16>()]
+                    .try_into()
+                    .unwrap(),
+            ) as usize;
 
-    let id = CkbAuthType {
-        algorithm_id: AuthAlgorithmIdType::try_from(auth_id).map_err(|f| CkbAuthError::from(f))?,
-        pubkey_hash: pubkey_hash,
-    };
+            if witness.len() < witness_offset + sign_size + size_of::<u16>() {
+                return Err(Error::ArgsError);
+            }
+            let sign = witness
+                [witness_offset + size_of::<u16>()..witness_offset + size_of::<u16>() + sign_size]
+                .to_vec();
+            witness_offset += size_of::<u16>() + sign_size;
+            sign
+        };
 
-    let entry = CkbEntryType {
-        code_hash,
-        hash_type,
-        entry_category: EntryCategoryType::try_from(entry_type)
-            .map_err(|f| CkbAuthError::from(f))
-            .unwrap(),
-    };
-
-    ckb_auth(&entry, &id, &signature, &message)?;
+        let id = CkbAuthType {
+            algorithm_id: AuthAlgorithmIdType::try_from(auth_id)
+                .map_err(|f| CkbAuthError::from(f))?,
+            pubkey_hash: pubkey_hash,
+        };
+        let entry = CkbEntryType {
+            code_hash,
+            hash_type,
+            entry_category: EntryCategoryType::try_from(entry_type)
+                .map_err(|f| CkbAuthError::from(f))
+                .unwrap(),
+        };
+        ckb_auth(&entry, &id, &signature, &message)?;
+    }
 
     Ok(())
 }
